ff4f768e1e207b89062775d78b65c7425c3cb0c9
FurrySparkle <97181304+FurrySparkle@users.noreply.github.com>
2023-12-21 21:21:25 -0700

    Added notes for tomorrow's work
    This adds most of what is needed to finish the converter on the theory side. Now I need to find the C++ language for it and the good user experience even for a jank release.

--- OpenPulseConverterSingleton.cpp
+++ OpenPulseConverterSingleton.cpp
@@ -18,17 +18,67 @@ int main(){
 
     char HidDeviceObject = "1915:EEE0" // Right Glove
 
-    BOOLEAN HidD_GetPreparsedData(
+    BOOLEAN HidD_GetPreparsedData(  // Not sure if this is working, needs preparsed data for getData function
   [in]  HANDLE               HidDeviceObject,
   [out] PHIDP_PREPARSED_DATA *PreparsedData
 );
+    printf("Please check your Right Glove is running data into this prompt \n");
+
+    if(!HidD_GetInputReport(HidDeviceObject, 01, 15)){ printf("ERROR NO GLOVE DETECTED, RESTART PROGRAM! \n"); return 1};
 
 
-    if(HidD_GetInputReport(HidDeviceObject, 01, 15)){
-        char data = HidP_GetData(HidP_Input, data, 15, *PreparsedData, 01, 15)
-    printf(&data);
+    while(HidD_GetInputReport(HidDeviceObject, 01, 15)){ //If glove report exists lets see it
+        char data = HidP_GetData(HidP_Input, data, 15, &PreparsedData, 01, 15)
+    printf(&data); // sent to command prompt
+    delay(14.97) //Delay of 14.97 ms to achieve 67Hz
     };
-    
+    system("pause");
+
+
+
+
+
+    printf("Please start SteamVR with OpenGloves running \n");
+    system("pause");
+
+    //NamedPipe stuff here
+
+    // "\\.\pipe\vrapplication\input\glove\v1\<left/right>"
+// struct InputData {
+//   const std::array<std::array<float, 4>, 5> flexion;
+//   const std::array<float, 5> splay;
+//   const float joyX;
+//   const float joyY;
+//   const bool joyButton;
+//   const bool trgButton;
+//   const bool aButton;
+//   const bool bButton;
+//   const bool grab;
+//   const bool pinch;
+//   const bool menu;
+//   const bool calibrate;
+// };
+
+// "\\.\pipe\vrapplication\input\glove\v2\<left/right>"
+struct InputData {  //We will only be using the Flexion and Splay everything else will be emulated by OpenGloves or within the UI for OG
+  const std::array<std::array<float, 4>, 5> flexion;
+  const std::array<float, 5> splay;
+  const float joyX;
+  const float joyY;
+  const bool joyButton;
+  const bool trgButton;
+  const bool aButton;
+  const bool bButton;
+  const bool grab;
+  const bool pinch;
+  const bool menu;
+  const bool calibrate;
+
+  const float trgValue;
+};
+
+
+
 
     return 0;
 };
@@ -45,3 +95,15 @@ int main(){
 // Encapsulate a 10 ms delay for bytes 2&3 to emulate duration
 // Byte2 is H x 255
 // Byte3 is F x 255
+
+
+//FFB LUCID INFO
+
+// "A" - Thumb force feedback. Integer from 0-1000
+// "B" - Index force feedback. Integer from 0-1000
+// "C" - Middle force feedback. Integer from 0-1000
+// "D" - Ring force feedback. Integer from 0-1000
+// "E" - Pinky force feedback. Integer from 0-1000
+// "F" - Frequency of haptic vibration. Decimal
+// "G" - Duration of haptic vibration. Decimal
+// "H" - Amplitude of haptic vibration. Decimal
